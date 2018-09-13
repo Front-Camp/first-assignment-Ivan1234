7e867a9c9a064ca33549da9fff69e61415f63fa2
Ivan Khuda <ivanchuda@gmail.com>
2018-09-13 20:17:16 +0300

    test is object

--- src/isObject/index.js
+++ src/isObject/index.js
@@ -8,7 +8,10 @@
  * isObject(null); // false
  */
 const isObject = data => {
-  /* your logic here...*/
+  if (Array.isArray(data)) return false;
+  if (data === null) return false;
+  if (typeof data === 'object') return true;
+  return false;
 };
 
 export default isObject;
